c6b27a48d3ec9a4d062c93b9bb0337614a705100
baggy102 <rlgh0806@gmail.com>
2023-08-15 17:51:04 +0900

    객체지향 프로그래밍 in JavaScript : 객체의 개념, this

---
+++ object.js
@@ -0,0 +1,9 @@
+let memberArray = ["baggy", "egoing", "leezhce"];
+
+let memberObject = {
+  manager: "egoing",
+  developer: "grgaphittie",
+  designer: "leezhce",
+};
+
+console.log(memberObject.designer);
